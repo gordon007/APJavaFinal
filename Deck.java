cda2f54f640a21a859dcbae75b6b701624c02d78
gordon007 <carsonlove17@hotmail.com>
2019-05-05 17:41:50 -0500

    Modified Deck Class
    I took the Deck classes and modified it from the AP lab's design... making a deck more reflect any stack of cards as well as a standard 52 card deck

--- Deck.java
+++ Deck.java
@@ -2,15 +2,20 @@ import java.util.ArrayList;
 
 public class Deck {
 
+	//holds current cards
 	ArrayList<Card> cards = new ArrayList<Card>();
-	ArrayList<Card> discards = new ArrayList<Card>();
+	//holds cards that were present at deck creation
+	ArrayList<Card> orgCards = new ArrayList<Card>();
 	
+	//Constructor to build standard 52 card deck, and others
 	public Deck(String[] ranks, String[]suits, int[] values)
 	{
+		//failsafe
 		boolean rankLessThanValue = false;
 		if(ranks.length < values.length)
 			rankLessThanValue = true;
 		
+		//generates deck
 		for(int i = 0; i < suits.length; i++)
 		{
 			if(rankLessThanValue)
@@ -28,8 +33,42 @@ public class Deck {
 				}
 			}
 		}
+		
+		//builds orgCards
+		for(int i = 0; i < cards.size(); i++)
+		{
+			orgCards.add(cards.get(i));
+		}
+	}
+
+	//Builds either standard or blank deck, depending on parameter
+	public Deck(boolean buildStandard)
+	{
+		if(buildStandard)
+		{
+			String[] ranks = {"ace", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "jack", "queen", "king"};
+			String[] suits = {"heart", "spade", "club", "diamond"};
+			int[] values = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13};
+			
+			//generates deck
+			for(int i = 0; i < suits.length; i++)
+			{
+				for(int j = 0; j < ranks.length; j++)
+				{
+					cards.add(new Card(ranks[j],suits[i],values[j]));
+				}
+			}
+			
+			//builds orgCards
+			for(int i = 0; i < cards.size(); i++)
+			{
+				orgCards.add(cards.get(i));
+			}
+		}
+		
 	}
 	
+	//returns true if no cards left
 	public boolean isEmpty()
 	{
 		if(cards.size() == 0)
@@ -37,35 +76,40 @@ public class Deck {
 		return false;
 	}
 	
+	//returns number of cards present
 	public int size()
 	{
 		return cards.size();
 	}
 	
+	//takes the first card from the deck, returns it
 	public Card deal()
 	{
 		if(isEmpty())
 			return null;
 		
-		Card grab = cards.get(0);
-		cards.remove(0);
-		discards.add(grab);
-		return grab;
+		return cards.remove(0);
 	}
 	
+	//shuffles cards present
 	public void shuffle()
-	{
-		while(discards.size() != 0)
-		{
-			cards.add(discards.remove(0));
-		}
-		
+	{	
 		for(int i = size()-1; i > 0; i--)
 		{
 			int r = (int) (Math.random() * i);
 			Card mm = cards.get(i);
 			cards.set(i,cards.get(r));
 			cards.set(r, mm);
+		}
+	}
+	
+	//resets the deck to original state
+	public void reset()
+	{
+		cards.clear();
+		for(int i = 0; i < orgCards.size(); i++)
+		{
+			cards.add(orgCards.get(i));
 		}
 	}
 	
